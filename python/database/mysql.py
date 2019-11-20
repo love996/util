5e8183d84856f7163014b75a1200064a4e4d5e98
yixuehan <505703394@qq.com>
2019-11-20 11:36:35 +0800

    kw

---
+++ python/database/mysql.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+keywords = [
+        # 类型
+        "char",
+        "varchar",
+        "int",
+        "smallint",
+
+        # 特殊关键字
+        "primary key",
+        "foreign key references",
+        "unique key"
+]
